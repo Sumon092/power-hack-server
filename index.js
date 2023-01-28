b5226565213da2695e54f8627a17130083fd6cff
Sumon <m.sumon@outlook.com>
2023-01-29 02:01:34 +0600

    Mongo connection error solved

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
 const port = process.env.PORT || 8000;
+require('dotenv').config();
 
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 
-const uri = "mongodb+srv://Power_hack_user:Gu33Y83xIl9IkLZN@cluster0.ys0dmn7.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.POWER_DB_USER}:${process.env.POWER_DB_PASS}@cluster0.ys0dmn7.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
