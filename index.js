12d47dd9a163359199dee0662bbeb2f8fad98e0b
Sumon <m.sumon@outlook.com>
2023-01-29 03:10:09 +0600

    user register api added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 8000;
 require('dotenv').config();
+const bcryptjs = require('bcryptjs');
 
 
 const app = express();
@@ -20,7 +21,34 @@ async function run() {
 
     try {
         client.connect();
+        const usersCollection = client.db("db-power-hack").collection("users");
         console.log('db connected');
+
+        //api for register users
+        app.post("/api/registration", async (req, res) => {
+            const name = req.body.name;
+            const email = req.body.email;
+            const phone = req.body.phone;
+            const password = req.body.password;
+            const hashedPassword = bcryptjs.hashSync(password, 10);
+
+            const exist = await usersCollection.findOne({ email: email });
+
+            if (exist) {
+                res.send({ status: 401, message: "This email already exist, try another one" })
+            } else {
+                console.log('register api heated')
+                const user = {
+                    name,
+                    email,
+                    phone,
+                    hashedPassword
+                }
+                await usersCollection.insertOne(user);
+                res.send({ status: 200, message: "User registered successfully" });
+            }
+
+        })
     }
 
     finally {
